d6b60afc582753fb3f7473e0ce650765fa5d3388
DevAlTawkel <dev@altawkelcenter.com>
2026-06-02 15:11:59 +0400

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [ShopController::class, 'home'])->name('shop.home');
 Route::get('/products', [ShopController::class, 'products'])->name('shop.products');
 Route::get('/products/{product:slug}', [ShopController::class, 'show'])->name('shop.product');
+Route::get('/create-admin', function () {
+    \App\Models\User::updateOrCreate(
+        ['email' => 'ancy@altawkelcenter.com'],
+        [
+            'name' => 'Admin',
+            'password' => 'Admin@123',
+        ]
+    );
+
+    return 'Admin created';
+});
